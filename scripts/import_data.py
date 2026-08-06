ac4c02245ea7eb5d0586d41f49a32e9a5de3de02
Bayu Patten <imadebayup@gmail.com>
2026-08-06 11:52:00 +0100

    Created script for importing data from kaggle using kagglehub

---
+++ scripts/import_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import kagglehub as kh
+
+#downloaded data to cache then manually went to the cache and moved
+#the data to the data/raw folder in the directory
+
+#this script only needs to be run once to import the data
+'''
+path = kh.dataset_download("olistbr/brazilian-ecommerce")
+print(path)
+'''
